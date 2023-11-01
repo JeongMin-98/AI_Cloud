1548249ad9a51bb00c4dc0b6c6ae5323c611c637
JeongMin-98 <jeongmin981@gmail.com>
2023-11-01 16:01:03 +0900

    [add] Complete Model Architecture

--- PytorchTutorial/model/model.py
+++ PytorchTutorial/model/model.py
@@ -2,8 +2,6 @@ import os
 import torch
 from torch import nn
 
-from ..Tensor.tensor import get_attributes_of_tensor
-
 
 def check_device():
     device = (
@@ -21,11 +19,25 @@ class MyCNN(nn.Module):
         super().__init__()
         self.layer = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=16, kernel_size=(3, 3), padding_mode="zeros"),
-            nn.ReLU()
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2,2)),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), padding_mode="zeros"),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding_mode="zeros"),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.Flatten(start_dim=0),
+            nn.Linear(in_features=73984, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=4),
+            nn.Softmax(0),
         )
+
     def forward(self, x):
         x = self.layer(x)
         return x
+
 
 if __name__ == "__main__":
     my_status_device = check_device()
@@ -34,7 +46,8 @@ if __name__ == "__main__":
     model = MyCNN().to(my_status_device)
     print(model)
 
-    input_feature_map = torch.randn(3,288,288)
-    output = model.forward(x)
-    get_attributes_of_tensor(output)
-
+    input_feature_map = torch.randn(3, 288, 288).to(my_status_device)
+    output = model.forward(input_feature_map)
+    print(f"Shape of tensor: {output.shape}")
+    print(f"Datatype of tensor: {output.dtype}")
+    print(f"Device tensor is stored on: {output.device}")
